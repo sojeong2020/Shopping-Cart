60c128ede7f19a87c00b9b8fca430a86b9edc32a
Sojeong Park <macpark09@gmail.com>
2021-10-07 16:31:16 +0100

    implemet removeFromCart function

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Basket = () => {
+const Basket = (props) => {
+    console.log(props,"<<< props from basket")
+    const {cartItems} =props;
     return (
         <div>
             <h1>Shopping Cart</h1>
-            
+            {cartItems.length === 0 && <p>Cart is empty!</p>}
         </div>
     );
 };

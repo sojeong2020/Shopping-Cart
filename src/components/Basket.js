50dd46fbe597a90f117f6b8088e92d6d555025ef
Sojeong Park <macpark09@gmail.com>
2021-10-07 16:36:25 +0100

    render each item checkout price

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -7,6 +7,21 @@ const Basket = (props) => {
         <div>
             <h1>Shopping Cart</h1>
             {cartItems.length === 0 && <p>Cart is empty!</p>}
+            <ul>
+            {cartItems.map(item=>{
+                 return(
+                     <li ley={item.id}>
+                     <h2>{item.name}</h2>
+                     <h2>Quentity: {item.qty}</h2>
+                     <h2>Price: {
+                         item.id === "1" ? item.qty * item.price+Math.floor(item.qty/3)*-20 : 
+                         item.id === "2" ? item.qty * item.price+Math.floor(item.qty/2)*-15 : item.qty * item.price
+                        } </h2>
+                         
+                    </li>
+                 )
+            })}  
+            </ul>
         </div>
     );
 };

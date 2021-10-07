3eae613f3a6741a0b37a3fcb5ecccf66578c7266
Sojeong Park <macpark09@gmail.com>
2021-10-07 16:51:19 +0100

    implement the code for calculating grand-total price

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -23,24 +23,19 @@ const Basket = (props) => {
             })}  
             </ul>
 
-            {/* <h2> TotalPayment: {
-             cartItems.reduce((a,c)=> {
-             return a + c.id === "1" ? c.qty * c.price+Math.floor(c.qty/3)*-20 : 
-             c.id === "2" ? c.qty * c.price+Math.floor(c.qty/2)*-15 : c.qty * c.price
-             } ,0)
-            }</h2> */}
-
-           {/*  <h2> Total: {
-            cartItems.map(item=> { 
-             if(item.id === "1"){
-                 return item.qty * item.price+Math.floor(item.qty/3)*-20 
-             }else if(item.id === "2") {
-                return item.qty * item.price+Math.floor(item.qty/2)*-15  
-             }else {
-                 return item.qty * item.price
-             }
-            })
-            }</h2>  */}
+            <h1>TotalPayment: {
+                 cartItems.length !== 0 &&
+                cartItems.map(item=> {
+                    if(item.id === "1"){
+                        return item.qty * item.price+Math.floor(item.qty/3)*-20 
+                    }else if(item.id === "2") {
+                        return item.qty * item.price+Math.floor(item.qty/2)*-15 
+                    }else {
+                        return item.qty * item.price
+                    }
+                }).reduce((a,c)=> a +c )
+                }
+            </h1>
 
             <h2> Total items : {
              cartItems.reduce((a,c)=> {
